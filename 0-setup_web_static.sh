31834b92d77ae16b6314f0a67b0db9b4c87b2ac3
lupamo <arnoldlupamo@gmail.com>
2024-04-04 16:37:12 +0300

    static deployment 1

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#script to deploy web static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+#creating directories
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+
+#creating a html file
+sudo echo "<html>
+<head>
+</head>
+<body>
+Holberton School
+<body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+#create or upadate the symbolic link
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+#changing ownership of /data/
+sudo chown -R ubuntu:ubuntu /data/
+
+#upddate nginx configuration to serve content from /data/web_static/current
+if ! sudo grep -q "alias /data/web_static/current/;" /etc/nginx/sites-enabled/default; then
+    sudo sed -i "\#server_name _;#a \\
+        location /hbnb_static { \\
+            alias /data/web_static/current/; \\
+        } \\
+        " /etc/nginx/sites-enabled/default
+fi
+
+# restart it
+sudo nginx -t
+sudo systemctl restart nginx
