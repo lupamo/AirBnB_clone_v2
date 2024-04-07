0063613c6e23dcc26e5d07fbd90ced4bc624abd0
lupamo <arnoldlupamo@gmail.com>
2024-04-07 05:42:19 -0700

    archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,11 +10,11 @@ from datetime import datetime
 
 def do_pack():
 	"""
-	archiving th web_static folder
+	archiving the web_static folder
 	"""
 
 	t = datetime.now()
-	achv = 'web_static_' * t.strftime("%Y%m%d%H%M%S") + "." + 'tgz'
+	achv = 'web_static_' + t.strftime("%Y%m%d%H%M%S") + "." + 'tgz'
 	local("mkdir -p versions")
 	build = local("tar -cvzf versions/{} web_static".format(achv))
 
