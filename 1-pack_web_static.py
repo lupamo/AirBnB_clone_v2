dc372b75f33b6cea1cecfa4666bc580dc7db061a
lupamo <arnoldlupamo@gmail.com>
2024-04-07 15:54:13 +0300

    pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Fabric script to generate a .tgz archive from the
-contents of the web_static 
+contents of the web_static
 """
 
 from fabric.api import *
@@ -9,16 +9,15 @@ from datetime import datetime
 
 
 def do_pack():
-	"""
-	archiving the web_static folder
-	"""
+    """
+    archiving the web_static folder
+    """
+    t = datetime.now()
+    achv = 'web_static_' + t.strftime("%Y%m%d%H%M%S") + "." + 'tgz'
+    local("mkdir -p versions")
+    build = local("tar -cvzf versions/{} web_static".format(achv))
 
-	t = datetime.now()
-	achv = 'web_static_' + t.strftime("%Y%m%d%H%M%S") + "." + 'tgz'
-	local("mkdir -p versions")
-	build = local("tar -cvzf versions/{} web_static".format(achv))
-
-	if build is not None:
-		return achv
-	else:
-		return None
+    if build is not None:
+        return achv
+    else:
+        return None
