ed56c8ede4e841aad918a084ee151a96e12ec16f
lupamo <arnoldlupamo@gmail.com>
2024-04-07 22:40:29 +0300

    deploy

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+a script that distributes an archive to your web servers,
+using the function do_deploy
+"""
+
+from datetime import datetime
+import os
+from fabric import *
+
+
+"""web-01, web-02 IPs"""
+env.hosts = ['100.25.190.224', '54.146.86.109']
+
+
+def do_deploy(archivePath):
+    """
+    deploys archive files to the webservers
+    """
+    if not os.path.exists(archivePath):
+        return False
+    fn = archivePath.split('/')[-1]
+    comp_file = '/data/web_static/releases/' + "{}".format(fn.split('.')[0])
+    tmp = "/tmp/" + fn
+
+    try:
+        """uploading archive file to tmp folder on server"""
+        put(archivePath, "/tmp/")
+        """
+        uncompress the file to the
+        /data/web_static/releases on the server
+        """
+        run("mkdir -p {}/".format(comp_file))
+        run("tar -xzf {} -C {}/".format(tmp, comp_file))
+        run("mv {}/web_static/* {}/".format(comp_file, comp_file))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data//web_static/current".format(comp_file))
+        return True
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
